6869914baabef2bb9818203dc6e38a321a34a6c4
Aram <dragolino6@mail.ru>
2020-08-14 21:27:53 +0300

    Updated English localization
    • Updated description for the role.

--- lua/lang/english/sidekick.lua
+++ lua/lang/english/sidekick.lua
@@ -3,6 +3,6 @@ L = LANG.GetLanguageTableReference("english")
 -- GENERAL ROLE LANGUAGE STRINGS
 L[SIDEKICK.name] = "Sidekick"
 L["target_" .. SIDEKICK.name] = "Sidekick"
-L["ttt2_desc_" .. SIDEKICK.name] = [[You need to win with your mate!]]
+L["ttt2_desc_" .. SIDEKICK.name] = [[You are now a Sidekick. Help your new teammate win the round!]]
 L["body_found_" .. SIDEKICK.abbr] = "They were a Sidekick."
 L["search_role_" .. SIDEKICK.abbr] = "This person was a Sidekick!"
