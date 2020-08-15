4da2c85e4c3c66ce7544d4c4f06ee6a5e37cdd0d
Tekiad <tekiadp@gmail.cm>
2020-08-15 12:49:57 -0300

    Create sidekick.lua (#20)

---
+++ lua/lang/spanish/sidekick.lua
@@ -0,0 +1,8 @@
+L = LANG.GetLanguageTableReference("Español")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[SIDEKICK.name] = "Socio"
+L["target_" .. SIDEKICK.name] = "Socio"
+L["ttt2_desc_" .. SIDEKICK.name] = [[¡Debes ganar con tu compañero!]]
+L["body_found_" .. SIDEKICK.abbr] = "¡Era un Socio!"
+L["search_role_" .. SIDEKICK.abbr] = "Esta persona era un Socio."
