e5cbd98b0d8e00f0c7bbc5cdc66d47e2616e95d5
Aram <dragolino6@mail.ru>
2020-08-15 18:50:10 +0300

    Updated English localization; Added Russian localization (#22)

---
+++ lua/lang/russian/sidekick.lua
@@ -0,0 +1,8 @@
+L = LANG.GetLanguageTableReference("russian")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[SIDEKICK.name] = "Сообщник"
+L["target_" .. SIDEKICK.name] = "Сообщник"
+L["ttt2_desc_" .. SIDEKICK.name] = [[Вы теперь сообщник! Помогите своему новому товарищу выиграть раунд!]]
+L["body_found_" .. SIDEKICK.abbr] = "Он был сообщником."
+L["search_role_" .. SIDEKICK.abbr] = "Этот человек был сообщником!"
