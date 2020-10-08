9019dbda8078b74a94a45e437114d1b7fd551d29
Tim Goll <tim@timgoll.de>
2020-10-08 18:29:47 +0200

    added missing language strings

--- lua/terrortown/lang/deutsch/sidekick.lua
+++ lua/terrortown/lang/deutsch/sidekick.lua
@@ -6,3 +6,12 @@ L["target_" .. SIDEKICK.name] = "Kumpane"
 L["ttt2_desc_" .. SIDEKICK.name] = [[Du musst mit deinem Mate gewinnen!]]
 L["body_found_" .. SIDEKICK.abbr] = "Er war ein Kumpane!"
 L["search_role_" .. SIDEKICK.abbr] = "Diese Person war ein Kumpane!"
+
+L["weapon_sidekickdeagle_name"] = "Kumpanendeagle"
+L["weapon_sidekickdeagle_desc"] = "Schieße auf einen Spieler, um ihn zu deinem Kumpanen zu machen."
+
+L["ttt2_siki_shot"] = "Erfolgreich {name} zu deinem Kumpanen geschossen!"
+L["ttt2_siki_were_shot"] = "Du wurdest von {name} zu einem Kumpanen geschossen!"
+L["ttt2_siki_sameteam"] = "Du kannst niemanden aus deinem eigenen Team zum Kumpanen schießen!"
+L["ttt2_siki_ply_killed"] = "Deine Sidekick Deagle Wartezeit wurde um {amount} Sekunden reduziert."
+L["ttt2_siki_recharged"] = "Deine Sidekick Deagle wurde wieder aufgefüllt."
--- lua/terrortown/lang/english/sidekick.lua
+++ lua/terrortown/lang/english/sidekick.lua
@@ -6,3 +6,12 @@ L["target_" .. SIDEKICK.name] = "Sidekick"
 L["ttt2_desc_" .. SIDEKICK.name] = [[You are now a Sidekick. Help your new teammate win the round!]]
 L["body_found_" .. SIDEKICK.abbr] = "They were a Sidekick."
 L["search_role_" .. SIDEKICK.abbr] = "This person was a Sidekick!"
+
+L["weapon_sidekickdeagle_name"] = "Sidekickdeagle"
+L["weapon_sidekickdeagle_desc"] = "Shoot a player to make him your Sidekick."
+
+L["ttt2_siki_shot"] = "Successfully shot {name} as your Sidekick!"
+L["ttt2_siki_were_shot"] = "You were shot as a Sidekick by {name}!"
+L["ttt2_siki_sameteam"] = "You can't shoot someone from your own team as Sidekick!"
+L["ttt2_siki_ply_killed"] = "Your Sidekick Deagle cooldown was reduced by {amount} seconds."
+L["ttt2_siki_recharged"] = "Your Sidekick Deagle has been recharged."
