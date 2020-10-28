702092ca6a912587a18c804f1ba7f27854fa5b8e
MisterClems <73431903+MisterClems@users.noreply.github.com>
2020-10-28 23:36:52 +0100

    Create sidekick.lua
    French translation : sidekick

---
+++ lua/terrortown/lang/french/sidekick.lua
@@ -0,0 +1,17 @@
+local L = LANG.GetLanguageTableReference("french")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[SIDEKICK.name] = "Acolyte"
+L["target_" .. SIDEKICK.name] = "Acolyte"
+L["ttt2_desc_" .. SIDEKICK.name] = [[Vous êtes un Acolyte. Aidez votre nouveau coéquipier à remporter la manche!]]
+L["body_found_" .. SIDEKICK.abbr] = "C'était un Acolyte."
+L["search_role_" .. SIDEKICK.abbr] = "Cette personne était un Acolyte!"
+
+L["weapon_sidekickdeagle_name"] = "Deagle de l'Acolyte"
+L["weapon_sidekickdeagle_desc"] = "Tirez sur un joueur pour qu'il devienne votre Acolyte."
+
+L["ttt2_siki_shot"] = "Votre tir a réussi à faire de {name} votre Acolyte!"
+L["ttt2_siki_were_shot"] = "Vous avez été converti en Acolyte par {name}!"
+L["ttt2_siki_sameteam"] = "Vous ne pouvez pas tirer sur quelqu'un de votre propre team en tant qu'Acolyte!"
+L["ttt2_siki_ply_killed"] = "Le rechargement de votre Deagle d'Acolyte a été réduit de {amount} secondes."
+L["ttt2_siki_recharged"] = "Votre Deagle d'Acolyte a été rechargé."
