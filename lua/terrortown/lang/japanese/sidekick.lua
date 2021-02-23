f3185fb5ffd942759b9217860224a8d1098fde85
westooooo <68987820+westooooo@users.noreply.github.com>
2021-02-23 15:03:46 +0900

    Add Japanese Translation

---
+++ lua/terrortown/lang/japanese/sidekick.lua
@@ -0,0 +1,17 @@
+local L = LANG.GetLanguageTableReference("japanese")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[SIDEKICK.name] = "サイドキック"
+L["target_" .. SIDEKICK.name] = "サイドキック"
+L["ttt2_desc_" .. SIDEKICK.name] = [[あなたはサイドキックだ。主人を守り、勝利へ導こう！]]
+L["body_found_" .. SIDEKICK.abbr] = "奴はサイドキックだったようだな。"
+L["search_role_" .. SIDEKICK.abbr] = "こいつはサイドキックだったようだな！"
+
+L["weapon_sidekickdeagle_name"] = "サイドキックイーグル"
+L["weapon_sidekickdeagle_desc"] = "誰かに撃つとその人が自分の仲間、サイドキックになる。"
+
+L["ttt2_siki_shot"] = "{name}をサイドキックにした！"
+L["ttt2_siki_were_shot"] = "{name}にサイドキックにされた！"
+L["ttt2_siki_sameteam"] = "サイドキックのように、自分の陣営の者の誰かは撃つことはできないぞ！"
+L["ttt2_siki_ply_killed"] = "サイドキックイーグルのクールダウン終了まで{amount}秒"
+L["ttt2_siki_recharged"] = "サイドキックイーグルがもう一回使えるようになったぞ。"
