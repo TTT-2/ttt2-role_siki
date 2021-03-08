3c08197e6187b89a7171010603ec93166a46dc09
westooooo <68987820+westooooo@users.noreply.github.com>
2021-03-07 21:23:26 -0900

    Update sidekick.lua

--- lua/terrortown/lang/japanese/sidekick.lua
+++ lua/terrortown/lang/japanese/sidekick.lua
@@ -1,17 +1,17 @@
 local L = LANG.GetLanguageTableReference("japanese")
 
 -- GENERAL ROLE LANGUAGE STRINGS
-L[SIDEKICK.name] = "サイドキック"
-L["target_" .. SIDEKICK.name] = "サイドキック"
-L["ttt2_desc_" .. SIDEKICK.name] = [[あなたはサイドキックだ。主人を守り、勝利へ導こう！]]
-L["body_found_" .. SIDEKICK.abbr] = "奴はサイドキックだったようだな。"
-L["search_role_" .. SIDEKICK.abbr] = "こいつはサイドキックだったようだな！"
+L[SIDEKICK.name] = "Sidekick"
+L["target_" .. SIDEKICK.name] = "Sidekick"
+L["ttt2_desc_" .. SIDEKICK.name] = [[あなたはSidekickだ。主人を守り、勝利へ導こう！]]
+L["body_found_" .. SIDEKICK.abbr] = "奴はSidekickだったようだな。"
+L["search_role_" .. SIDEKICK.abbr] = "こいつはSidekickだったようだな！"
 
-L["weapon_sidekickdeagle_name"] = "サイドキックイーグル"
-L["weapon_sidekickdeagle_desc"] = "誰かに撃つとその人が自分の仲間、サイドキックになる。"
+L["weapon_sidekickdeagle_name"] = "Sidekick Deagle"
+L["weapon_sidekickdeagle_desc"] = "誰かに撃つとその人が自分の仲間、Sidekickになる。"
 
-L["ttt2_siki_shot"] = "{name}をサイドキックにした！"
-L["ttt2_siki_were_shot"] = "{name}にサイドキックにされた！"
-L["ttt2_siki_sameteam"] = "サイドキックのように、自分の陣営の者の誰かは撃つことはできないぞ！"
-L["ttt2_siki_ply_killed"] = "サイドキックイーグルのクールダウン終了まで{amount}秒"
-L["ttt2_siki_recharged"] = "サイドキックイーグルがもう一回使えるようになったぞ。"
+L["ttt2_siki_shot"] = "{name}をSidekickにした！"
+L["ttt2_siki_were_shot"] = "{name}にSidekickにされた！"
+L["ttt2_siki_sameteam"] = "Sidekickのように、自分の陣営の者の誰かは撃つことはできないぞ！"
+L["ttt2_siki_ply_killed"] = "Sidekickのクールダウン終了まで{amount}秒"
+L["ttt2_siki_recharged"] = "Sidekick Deagleがもう一回使えるようになったぞ。"
