fb0b15ad9b4e813ce6013a273e9eacf607a6edd9
Talha Berkay Akbulut <49911006+NovaDiablox@users.noreply.github.com>
2023-12-08 19:00:36 +0300

    Added Turkish Language (#30)
    Added Turkish Language

---
+++ lua/terrortown/lang/tr/sidekick.lua
@@ -0,0 +1,17 @@
+local L = LANG.GetLanguageTableReference("tr")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[SIDEKICK.name] = "Yardımcı"
+L["target_" .. SIDEKICK.name] = "Yardımcı"
+L["ttt2_desc_" .. SIDEKICK.name] = [[Şimdi bir Yardımcısın. Yeni takım arkadaşınızın raundu kazanmasına yardım et!]]
+L["body_found_" .. SIDEKICK.abbr] = "Onlar bir Yardımcıydı."
+L["search_role_" .. SIDEKICK.abbr] = "Bu kişi bir Yardımcıydı!"
+
+L["weapon_sidekickdeagle_name"] = "Yardımcı Deagle'ı"
+L["weapon_sidekickdeagle_desc"] = "Bir oyuncuyu Yardımcın yapmak için vurun."
+
+L["ttt2_siki_shot"] = "{name} adlı oyuncuyu Yardımcın olarak başarıyla vurdun!"
+L["ttt2_siki_were_shot"] = "{name} tarafından bir Yardımcı olarak vuruldun!"
+L["ttt2_siki_sameteam"] = "Kendi takımından birini Yardımcın olarak vuramazsın!"
+L["ttt2_siki_ply_killed"] = "Yardımcı Deagle bekleme süreniz {amount} saniye azaltıldı."
+L["ttt2_siki_recharged"] = "Yardımcı Deagle'ınız yeniden doldu."
