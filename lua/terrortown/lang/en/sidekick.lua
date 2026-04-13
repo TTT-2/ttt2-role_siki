2276ae2c28ae5aa42313bf91905d63fd759fdbb8
mexikoedi <mexikoedi@outlook.com>
2026-04-13 17:40:27 +0200

    f1 menu integration + convar for preventFindCredits

--- lua/terrortown/lang/en/sidekick.lua
+++ lua/terrortown/lang/en/sidekick.lua
@@ -7,6 +7,7 @@ L["ttt2_desc_" .. SIDEKICK.name] = [[You are now a Sidekick. Help your new teamm
 L["body_found_" .. SIDEKICK.abbr] = "They were a Sidekick."
 L["search_role_" .. SIDEKICK.abbr] = "This person was a Sidekick!"
 
+-- OTHER ROLE LANGUAGE STRINGS
 L["weapon_sidekickdeagle_name"] = "Sidekickdeagle"
 L["weapon_sidekickdeagle_desc"] = "Shoot a player to make him your Sidekick."
 
@@ -15,3 +16,19 @@ L["ttt2_siki_were_shot"] = "You were shot as a Sidekick by {name}!"
 L["ttt2_siki_sameteam"] = "You can't shoot someone from your own team as Sidekick!"
 L["ttt2_siki_ply_killed"] = "Your Sidekick Deagle cooldown was reduced by {amount} seconds."
 L["ttt2_siki_recharged"] = "Your Sidekick Deagle has been recharged."
+
+L["label_siki_preventFindCredits"] = "Prevent Sidekicks from finding credits in bodies"
+L["label_siki_protection_time"] = "Protection Time for new Sidekick"
+L["help_siki_mode"] = [[
+What happens when a player becomes a Sidekick?
+
+Mode 0: Sidekick doesn't become his former teammate and can't win alone, but gets targets.
+Mode 1: Sidekick becomes his former teammate upon their death. 
+Mode 2: Sidekick doesn't become his former teammate but can win alone and gets targets.]]
+L["label_siki_mode"] = "Which mode should be active?"
+L["label_siki_mode_0"] = "Mode 0"
+L["label_siki_mode_1"] = "Mode 1"
+L["label_siki_mode_2"] = "Mode 2"
+L["label_siki_deagle_refill"] = "The Sidekick Deagle can be refilled when you missed a shot."
+L["label_siki_deagle_refill_cd"] = "Seconds to Refill"
+L["label_siki_deagle_refill_cd_per_kill"] = "CD Reduction per Kill"
--- lua/terrortown/lang/it/sidekick.lua
+++ lua/terrortown/lang/it/sidekick.lua
@@ -7,6 +7,7 @@ L["ttt2_desc_" .. SIDEKICK.name] = [[Devi vincere con il tuo compagno!]]
 L["body_found_" .. SIDEKICK.abbr] = "Era un Aiutante..."
 L["search_role_" .. SIDEKICK.abbr] = "Questa persona era un Aiutante!"
 
+-- OTHER ROLE LANGUAGE STRINGS
 L["weapon_sidekickdeagle_name"] = "Deagle Aiutante"
 L["weapon_sidekickdeagle_desc"] = "Spara ad una persona per farla diventare tuo Aiutante."
 
@@ -15,3 +16,19 @@ L["ttt2_siki_were_shot"] = "Sei stato convertito come Aiutante da {name}!"
 L["ttt2_siki_sameteam"] = "Non puoi convertire qualcuno del tuo stesso team come Aiutante!"
 L["ttt2_siki_ply_killed"] = "E' stato ridotto il tempo di ricarica della Deagle Aiutante di {amount}s."
 L["ttt2_siki_recharged"] = "La tua Deagle Aiutante si è ricaricata."
+
+--L["label_siki_preventFindCredits"] = "Prevent Sidekicks from finding credits in bodies"
+--L["label_siki_protection_time"] = "Protection Time for new Sidekick"
+--L["help_siki_mode"] = [[
+--What happens when a player becomes a Sidekick?
+
+--Mode 0: Sidekick doesn't become his former teammate and can't win alone, but gets targets.
+--Mode 1: Sidekick becomes his former teammate upon their death. 
+--Mode 2: Sidekick doesn't become his former teammate but can win alone and gets targets.]]
+--L["label_siki_mode"] = "Which mode should be active?"
+--L["label_siki_mode_0"] = "Mode 0"
+--L["label_siki_mode_1"] = "Mode 1"
+--L["label_siki_mode_2"] = "Mode 2"
+--L["label_siki_deagle_refill"] = "The Sidekick Deagle can be refilled when you missed a shot."
+--L["label_siki_deagle_refill_cd"] = "Seconds to Refill"
+--L["label_siki_deagle_refill_cd_per_kill"] = "CD Reduction per Kill"
